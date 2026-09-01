5f57861a1c27674f122f70f5435904ed5d11bdfd
tommy <tommy@192.168.1.197>
2026-09-01 09:18:30 +0100

    ADD comment to get pattern

--- WarmupExercises/src/warmup/GetPattern.java
+++ WarmupExercises/src/warmup/GetPattern.java
@@ -4,13 +4,17 @@ public class GetPattern {
 	
 	public static void getPatternResulr() {
 		
+		//initial value for pattern repeat
 		int number = 5;
 		
+		//FOR loop to create the number of rows
 		for(int row = 1; row <= number; row++) {
-			
+			//NESTED loop to create number of columns
 			for(int column = 1; column <= number ; column++) {
+				//populate the row with a "# " per iteration 
 				System.out.print("# ");
 			}
+			//new line per iteration
 			System.out.println();
 		}
 	}
